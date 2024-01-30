2744f05caa4f1e884ef9ff934bd7bad2e9950eac
David Bauer <mail@david-bauer.net>
2024-01-30 19:15:46 +0100

    fixup! p1020rdb-pd: alter for MSM460

--- include/configs/p1_p2_rdb_pc.h
+++ include/configs/p1_p2_rdb_pc.h
@@ -994,7 +994,8 @@ i2c mw 18 3 __SW_BOOT_MASK 1; reset
 "ethaddr=02:04:9f:02:01:03\0"	\
 "boot_nand=mtdparts default && ubi part ubi && ubi read $loadaddr kernel && bootm $loadaddr\0"	\
 "boot_tftp=tftpboot $loadaddr msm460-initramfs.bin && bootm $loadaddr\0"	\
-"flash_ubi=mtdparts default && tftpboot $loadaddr msm460-factory.bin && nand erase.part ubi && nand write $loadaddr ubi $filesize\0"
+"flash_ubi=mtdparts default && tftpboot $loadaddr msm460-factory.bin && nand erase.part ubi && nand write $loadaddr ubi $filesize\0"	\
+"fdt_high=0x3000000\0"
 
 #if 0
 #define CONFIG_NFSBOOTCOMMAND	\
